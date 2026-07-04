3c8d738398b429c0fc1163fe5620404a694d5883
Victory Mwendwa <mwendwavictory29@gmail.com>
2026-07-04 11:55:54 +0300

    added validateUser and login functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {JwtService} from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser({username, password}: LoginDto){
+    const user = await this.usersService.findOne(username, true);
+    if (!user){
+      return null; // returning 'null' instead of throwing an exeption coz I want to use this method elsewhere
+    }
+
+    try {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch) {
+        return null;
+      }
+      
+    } catch (error) {
+      return null;
+    }
+
+    delete user.password;
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
