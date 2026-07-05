559560e8ef2cc56379d73337fbd5478cf152b36b
Victory Mwendwa <mwendwavictory29@gmail.com>
2026-07-05 18:59:55 +0300

    added a user login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,5 +31,11 @@ export class AuthService {
 
     return user;
   }
+   async login(user: any) {
+    const payload = (username: user.username, sub: user.id);
+    return (
+      access_token: this.jwtService.sign(payload),
+    );
+   }
 
 }
