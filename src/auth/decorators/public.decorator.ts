615a2611581d7ccafe093e496a97aaadd63e52c8
Victory Mwendwa <mwendwavictory29@gmail.com>
2026-07-08 11:24:13 +0300

    added a decorator to mark a route as public

--- src/auth/decorators/public.decorator.ts
+++ src/auth/decorators/public.decorator.ts
@@ -1,4 +1,5 @@
 import {SetMetadata} from '@nestjs/common';
 
+// this decorator is used to mark a route as public, meaning it does not require authentication. It sets a metadata key 'isPublic' to true, which can be checked by guards to allow access without authentication.
 export const IS_PUBLIC_KEY = 'isPublic'; // key for the metadata
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); // makes a route (method or class) public by setting the metadata key 'isPublic' to true. This decorator can be applied to controller methods or classes to indicate that they do not require authentication.
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); 
